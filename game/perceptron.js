67455a60f80f8112aac70160fef79e446e31b3ee
Douglas Carlini <douglascarlini@gmail.com>
2019-07-02 14:24:16 -0300

    Ajustes no Perceptron

--- game/perceptron.js
+++ game/perceptron.js
@@ -1,22 +1,23 @@
 (function () {
 
-    var self = function () {
+    var self = function (conf = {}) {
 
-        this.epocas = 10000;
-        this.apredizado = 0.5;
-        this.entradas = [{ x: [0, 0], y: 0 }];
-        this.ativar = (u) => { return (1 / (1 + Math.exp(-u))); };
-        this.pesos = [...this.entradas[0].x.map(i => Math.random())];
-        this.ajustar = function (e, x) { x.map((v, i) => { this.pesos[i] += this.apredizado * e * x[i] }); };
+        this.pesos = [];
+        this.entradas = [];
+        this.epocas = conf.epocas || 9999;
+        this.aprendizado = conf.aprendizado || 0.2;
+        this.ativar = function (u) { return (1 / (1 + Math.exp(-u))); };
+        this.ajustar = function (e, x) { x.map((v, i) => { this.pesos[i] += this.aprendizado * e * x[i] }); };
         this.testar = function (x) { var u = 0; x.map((v, i) => { u += this.pesos[i] * x[i] }); return this.ativar(u + 1); };
 
-        this.treinar = function (x) {
+        this.treinar = function (dataset = [{ x: [0, 0], y: 0 }]) {
+            this.pesos = [...dataset[0].x.map(i => Math.random())];
             for (let epoca = 1; epoca < this.epocas; epoca++) {
-                x.map((v, i) => {
-                    var valor = this.testar(x[i].x);
-                    if (valor != x[i].y) {
-                        var erro = x[i].y - valor;
-                        this.ajustar(erro, x[i].x);
+                dataset.map((v, i) => {
+                    var val = this.testar(dataset[i].x);
+                    if (val != dataset[i].y) {
+                        var erro = dataset[i].y - val;
+                        this.ajustar(erro, dataset[i].x);
                     }
                 });
             }
